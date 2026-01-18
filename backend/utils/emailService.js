46b7205fd93abc24e348eb3674834960e4c5c0fa
Essa Al Busaidi <116034947+BusaidiEssa@users.noreply.github.com>
2026-01-18 23:12:11 +0400

    Update emailService.js
    changed email port

--- backend/utils/emailService.js
+++ backend/utils/emailService.js
@@ -8,7 +8,7 @@ dotenv.config()
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
   port: parseInt(process.env.EMAIL_PORT),
-  secure: false,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
